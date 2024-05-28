64f2b13ca1f880b133e14b2cee9f6d06795fddfd
genaropx <165018329+genaropx@users.noreply.github.com>
2024-05-28 07:35:48 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.showString("placar")
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        . # # # .
+        `)
+    basic.pause(100)
+})
